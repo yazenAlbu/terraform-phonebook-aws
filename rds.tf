d36ff70fe62ad93cadfe35d9feb09ed4483f68ee
Yazen Albu <yazen.ishtar@gmail.com>
2026-09-22 09:57:38 +0200

    Add rds.tf

---
+++ rds.tf
@@ -0,0 +1,36 @@
+resource "aws_db_subnet_group" "phonebook" {
+  name       = "${var.project_name}-db-subnet-group"
+  subnet_ids = data.aws_subnets.default.ids
+
+  tags = {
+    Name = "${var.project_name}-db-subnet-group"
+  }
+}
+
+resource "aws_db_instance" "phonebook" {
+  identifier = "${var.project_name}-db"
+
+  engine         = "mysql"
+  engine_version = "8.0"
+  instance_class = var.db_instance_class
+
+  allocated_storage = var.db_allocated_storage
+  db_name           = var.db_name
+  username          = var.db_username
+  password          = var.db_password
+  port              = 3306
+
+  db_subnet_group_name   = aws_db_subnet_group.phonebook.name
+  vpc_security_group_ids = [aws_security_group.db.id]
+
+  publicly_accessible    = false
+  multi_az               = false
+  backup_retention_period = 0
+  skip_final_snapshot    = true
+  deletion_protection    = false
+  apply_immediately      = true
+
+  tags = {
+    Name = "${var.project_name}-db"
+  }
+}
